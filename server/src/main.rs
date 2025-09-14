b6c7502a46c8306f6721ed8429b6ab06b5a99fd1
Armin Hueneburg <hueneburg.armin@gmail.com>
2025-09-15 00:58:20 +0900

    Add cache etc to server

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,34 +2,56 @@ mod patient;
 
 use crate::patient::{Patient, PatientSearch, PatientUpdate};
 use axum::{extract::{Path, Query}, routing::{get, post, put}, Json, Router};
+use deadpool::Runtime;
 use tokio_postgres::{Error, NoTls};
+use uuid::Uuid;
 
 #[tokio::main]
 async fn main() {
     db_setup().await.unwrap();
+    setup_cache().await.unwrap();
     let app = Router::new().route("/fhir/Patient/{patient_id}", get(get_patient))
                            .route("/fhir/Patient", post(create_patient))
                            .route("/fhir/Patient", get(search_patients))
                            .route("/fhir/Patient/{patient_id}", put(update_patient));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
 async fn db_setup() -> Result<(), Error> {
-    let (client, connection) = tokio_postgres::connect("host=192.168.10.109 user=myuser dbname=fhir password=mypassword", NoTls).await?;
-    tokio::spawn(async move {
-        if let Err(e) = connection.await {
-            eprintln!("connection error: {}", e);
-        }
-    });
+    let mut cfg = tokio_postgres::Config::new();
+    cfg.dbname("fhir".to_string())
+       .host("127.0.0.1".to_string())
+       .password("mypassword".to_string())
+       .user("myuser".to_string());
+    let mgrCfg = deadpool_postgres::ManagerConfig {
+        recycling_method: deadpool_postgres::RecyclingMethod::Fast,
+    };
+
+    let mgr = deadpool_postgres::Manager::from_config(cfg, NoTls, mgrCfg);
+    let pool = deadpool_postgres::Pool::builder(mgr).max_size(16).build().unwrap();
+
+    let client = pool.get().await.unwrap();
+
     let rows = client.query("SELECT * FROM element;", &[]).await?;
 
-    let value: &str = rows[0].get(0);
-    print!("DB connection set up");
-    print!("{}", value);
+    let value: String = rows.into_iter()
+                            .map(|row| row.get::<usize, Uuid>(0).to_string())
+                            .collect::<Vec<_>>()
+                            .join(", ");
+    println!("PostgreSQL DB connection set up");
+    println!("{}", value);
 
-    return Result::Ok(());
+    return Ok(());
+}
+
+async fn setup_cache() -> Result<(), Error> {
+    let mut cfg = deadpool_redis::Config::from_url("redis://default:mysecretpassword@127.0.0.1:6379");
+    let pool = cfg.create_pool(Some(Runtime::Tokio1)).unwrap();
+    let mut conn = pool.get().await.unwrap();
+    deadpool_redis::redis::cmd("SET").arg(&["testkey", "42", "ex", "300"]).query_async::<()>(&mut conn).await.unwrap();
+    return Ok(());
 }
 
 async fn get_patient(Path(patient_id): Path<String>) -> String {
